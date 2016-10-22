5402fc9814114fa93e601002ee0e47e8f436c87f
Cheryl <cherylschaefer@gmail.com>
2016-10-22 18:12:42 -0500

    set up hiding later questions until previous question is answered

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,8 +1,22 @@
 $(document).ready( function () {
+
+  $(".later-questions").hide();
+
   $('input[name="intake_form[immediate_danger]"]').click(function() {
     var value = $(this).val();
     if (value == "Yes!") {
       window.location.assign("http://localhost:3000/results/travis");
     }
+    if (value == "No") {
+      $("#name-question").show();
+    }
   });
+
+  $('input[name="intake_form[name]"]').change(function() {
+    var value = $(this).val();
+    if (value.length > 0) {
+      alert("value is there");
+    }
+  });
+
 });
