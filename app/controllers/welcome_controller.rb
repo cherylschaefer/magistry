7d825c276b9c04804a244ddc5a28b4e7a3955302
Cheryl <cherylschaefer@gmail.com>
2016-10-22 14:24:27 -0500

    added routes and views for the 3 results

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,13 @@
 class WelcomeController < ApplicationController
+  def index
+  end
+
+  def scott
+  end
+
+  def sheila
+  end
+
+  def travis
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "results/sheila", to: 'welcome#sheila'
+  get "results/scott", to: 'welcome#scott'
+  get "results/travis", to: 'welcome#travis'
+
   root 'welcome#index'
 end
