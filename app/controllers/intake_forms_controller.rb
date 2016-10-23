177c4250f7f4cfc1b9c8ffc626f0c9fbc05f9198
Cheryl <cherylschaefer@gmail.com>
2016-10-22 19:15:26 -0500

    removing demographic info from results

--- app/controllers/intake_forms_controller.rb
+++ app/controllers/intake_forms_controller.rb
@@ -2,6 +2,8 @@ class IntakeFormsController < ApplicationController
 
   def create
     @intake_form = IntakeForm.new(intake_form_params)
+    # For a real app, we'd have a db of likely services to show that would be tagged based on their individual answers.
+    # this'll show the idea for the weekend
     if @intake_form.save
       if @intake_form.name == "Sheila" || @intake_form.name == "sheila"
         redirect_to results_sheila_path
